6c357315f9852b4031dd87f8fb6a163c20f49b01
stajyermsabirli <stajyer.msabirli@sampas.com.tr>
2025-05-16 11:42:52 +0300

    temel entityler oluşturuldu

---
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Detail: EntityBase
+    {
+        public Detail()
+        {
+
+        }
+
+        public Detail(string title, string description,int categoryId)
+        {
+            Title = title;
+            Description = description;
+            categoryId = categoryId;
+        }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public   MyProperty { get; set; }
+
+    }
+}
